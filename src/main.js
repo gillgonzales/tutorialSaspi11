b178d87926edf2322080e47bc861388379a4095e
Ryzen from R900 <echos999@gmail.com>
2023-11-08 18:15:55 -0300

    refactor: add GAME object

--- src/main.js
+++ src/main.js
@@ -3,60 +3,62 @@ import SkyBox from './classes/SkyBox'
 import TextureAnimator from './classes/TextureAnimator'
 import Model3D from './classes/Model3D';
 
-const QTD_ENEMIES = 5
-const HIT_RADIUS = .125
-let TOTAL_SHOTS = 1000
-let GAME_OVER = false
-let LOST_ENEMIES = 0
-let SCORE = 0
+const GAME = {
+	QTD_ENEMIES:  5,
+	HIT_RADIUS:   .125,
+	TOTAL_SHOTS:  1000,
+	GAME_OVER:    false,
+  LOST_ENEMIES: 0,
+	SCORE:        0
+}
 
-const renderer = new THREE.WebGLRenderer({ antialias: true })
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.querySelector('#game').appendChild(renderer.domElement)
+GAME.renderer = new THREE.WebGLRenderer({ antialias: true })
+GAME.renderer.setSize(window.innerWidth, window.innerHeight)
+document.querySelector('#game').appendChild(GAME.renderer.domElement)
 
-const scene = new THREE.Scene()
-scene.background = new THREE.Color(0x000000)
+GAME.scene = new THREE.Scene()
+GAME.scene.background = new THREE.Color(0x000000)
 
-const camera = new THREE.PerspectiveCamera(
+GAME.camera = new THREE.PerspectiveCamera(
   40, //campo de visao vertical 
   window.innerWidth / window.innerHeight, //aspecto da imagem (Largura/Altura)
   0.1, //Plano proximo
   1000 //Plano distante
 );
-camera.position.set(0, .25, 2)
+GAME.camera.position.set(0, .25, 2)
 
-const skyBox = new SkyBox('bluesky', 200)
-await skyBox.create(scene);
+GAME.skyBox = new SkyBox('bluesky', 200)
+GAME.skyBox.create(GAME.scene);
 
 const modelsPath = 'models/f15c/'
 
 const jetModel = new Model3D(modelsPath,'f15c.mtl','f15c.obj')
-const jet = await jetModel.create(scene)
+const jet = await jetModel.create(GAME.scene)
 const jetJoystick = { x: null, y: null }
 
 jet.scale.setScalar(.5)
 jet.position.y = -.2
 jet.shots = new Array()
 
-const light = new THREE.AmbientLight(0xffffff, 10);
-scene.add(light);
+GAME.light = new THREE.AmbientLight(0xffffff, 10);
+GAME.scene.add(GAME.light);
 
-const plight = new THREE.PointLight(0xffffff, 500, 50,.5);
-plight.position.set(0, 25, 10);
-scene.add(plight);
+GAME.plight = new THREE.PointLight(0xffffff, 500, 50,.5);
+GAME.plight.position.set(0, 25, 10);
+GAME.scene.add(GAME.plight);
 
 const enemyModel = new Model3D(modelsPath,'f15c_e.mtl','f15c.obj')
-const enemy = await enemyModel.create(scene)
+const enemy = await enemyModel.create(GAME.scene)
 enemy.scale.setScalar(.5)
 enemy.position.y = .4
 enemy.rotateY(3.14)
 // enemy.position.z = -1
 // scene.add(enemy)
 
-const sphere_geometry = new THREE.SphereGeometry(HIT_RADIUS / 2, 64, 32);
+const sphere_geometry = new THREE.SphereGeometry(GAME.HIT_RADIUS / 2, 64, 32);
 const sphereColor = new THREE.MeshStandardMaterial({ color: 0xffff00 });
 const sphere = new THREE.Mesh(sphere_geometry, sphereColor);
-const hitSphere = new THREE.Sphere(new THREE.Vector3(0, 0, 0), HIT_RADIUS)
+const hitSphere = new THREE.Sphere(new THREE.Vector3(0, 0, 0), GAME.HIT_RADIUS)
 
 const explosionTexture = new THREE.TextureLoader().load('img/textures/explosion.png');
 const explosionlight = new THREE.PointLight(0xff3300, 1, 5);
@@ -64,17 +66,17 @@ const explosionlight = new THREE.PointLight(0xff3300, 1, 5);
 const enemies = createEnemies()
 
 function shooting() {
-  if (TOTAL_SHOTS > 0) {
+  if (GAME.TOTAL_SHOTS > 0) {
     if (jet.shots.length > 50)
       return 0
-    TOTAL_SHOTS--
+      GAME.TOTAL_SHOTS--
     const shot = {
       rx: jet.rotation.z,
       ry: jet.rotation.x,
       model: sphere.clone(),
       hit: hitSphere.clone(),
     }
-    shot.hit.radius = HIT_RADIUS / 2
+    shot.hit.radius = GAME.HIT_RADIUS / 2
     shot.model.material.transparent = true
     shot.model.material.opacity = .5
     shot.model.material.emissive = new THREE.Color(0xffff00)
@@ -82,7 +84,7 @@ function shooting() {
     shot.model.material.metalness = 1
     shot.model.position.set(...jet.position)
     shot.hit.center.copy(shot.model.position)
-    scene.add(shot.model)
+    GAME.scene.add(shot.model)
     jet.shots.push(shot)
   } else {
     console.warn("ACABOU A MUNIÇÃO!!!")
@@ -105,7 +107,7 @@ function updateShots() {
 
   jet.shots = jet.shots.filter((shot) => {
     if (shot.model.position.z < -150) {
-      scene.remove(shot.model)
+      GAME.scene.remove(shot.model)
       return false
     }
     return true
@@ -115,7 +117,7 @@ function updateShots() {
 function createEnemies() {
   let distance = 100
   let horizontalLimit = 5
-  return Array.from({ length: QTD_ENEMIES }).map(() => {
+  return Array.from({ length: GAME.QTD_ENEMIES }).map(() => {
     let texture = explosionTexture.clone();
     texture.needsUpdate = true;
     let explodeMaterial = new THREE.MeshBasicMaterial({ map: texture, transparent: true });
@@ -138,7 +140,7 @@ function createEnemies() {
         model: new THREE.Mesh(exploGeo, explodeMaterial),
       }
     }
-    scene.add(enemyClone.model)
+    GAME.scene.add(enemyClone.model)
     return enemyClone
   })
 }
@@ -147,8 +149,8 @@ function showEnemyHit(enemy) {
   enemy.explosion.model.position.copy(enemy.model.position.clone())
   explosionlight.position.copy(enemy.model.position.clone())
   enemy.explosion.model.scale.setScalar(1.5)
-  scene.add(explosionlight);
-  scene.add(enemy.explosion.model);
+  GAME.scene.add(explosionlight);
+  GAME.scene.add(enemy.explosion.model);
   enemy.dead = true
 }
 
@@ -157,8 +159,8 @@ function moveEnemy(enemy) {
   let distance = enemy.model.position.z
   if (!enemy.dead) {
     if (distance > 30) {
-      LOST_ENEMIES++
-      console.warn('Inimigos perdidos:',LOST_ENEMIES)
+      GAME.LOST_ENEMIES++
+      console.warn('Inimigos perdidos:',GAME.LOST_ENEMIES)
       enemy.model.position.z = -(Math.random() * 100 + 100)
       enemy.model.position.x = Math.random() * (Math.random() > .5 ? 5 : -5);
     } else if (distance > -40) {
@@ -177,7 +179,7 @@ function moveEnemy(enemy) {
     if (enemy.explosion.sprite.currentTile < enemy.explosion.sprite.numberOfTiles - 1) {
       enemy.explosion.sprite.update(20)
     } else {
-      scene.remove(enemy.explosion.model);
+      GAME.scene.remove(enemy.explosion.model);
       enemy.dead = false
       enemy.model.position.z = -(Math.random() * 100 + 100)
       enemy.explosion.sprite.reset()
@@ -222,12 +224,12 @@ function moveJet() {
 }
 
 function isFinish() {
-  if (LOST_ENEMIES > 2 * QTD_ENEMIES || TOTAL_SHOTS <= 0) {
-    console.error('GAMEOVER!!!', SCORE, TOTAL_SHOTS, LOST_ENEMIES)
+  if (GAME.LOST_ENEMIES > 2 * GAME.QTD_ENEMIES || GAME.TOTAL_SHOTS <= 0) {
+    console.error('GAMEOVER!!!', GAME.SCORE, GAME.TOTAL_SHOTS, GAME.LOST_ENEMIES)
     console.table({
-      'PONTOS': SCORE,
-      'MUNIÇÃO': TOTAL_SHOTS,
-      'INIMIGOS PERDIDOS': LOST_ENEMIES
+      'PONTOS': GAME.SCORE,
+      'MUNIÇÃO': GAME.TOTAL_SHOTS,
+      'INIMIGOS PERDIDOS': GAME.LOST_ENEMIES
     })
     showModal()
     return true
@@ -239,32 +241,38 @@ function showModal(){
   //mostra modal de statisticas
   document.querySelector('#stats-modal').style.display='block'
   document.querySelector('#stats-results').innerHTML=`
-    PONTOS: ${SCORE} | MUNIÇÃO: ${TOTAL_SHOTS} | INIMIGOS PERDIDOS: ${LOST_ENEMIES}`
+    PONTOS: ${GAME.SCORE} | MUNIÇÃO: ${GAME.TOTAL_SHOTS} | INIMIGOS PERDIDOS: ${GAME.LOST_ENEMIES}`
 }
 
 const gameLoop = () => {
-  skyBox.model.rotation.y += .0001
-  skyBox.model.position.z += .0001
+  GAME.skyBox.model.rotation.y += .0001
+  GAME.skyBox.model.position.z += .0001
   moveJet()
   updateShots()
   enemies.forEach((e) =>{ 
     moveEnemy(e)
     if(!e.dead && shootDown(e)){
       showEnemyHit(e)
-      SCORE+=10
-      console.info('Pontos:',SCORE)
+      GAME.SCORE+=10
+      console.info('Pontos:',GAME.SCORE)
     }
   })
-  renderer.render(scene, camera)
-  GAME_OVER = isFinish()
-  !GAME_OVER && requestAnimationFrame(gameLoop)
+  GAME.renderer.render(GAME.scene, GAME.camera)
+  GAME.GAME_OVER = isFinish()
+  !GAME.GAME_OVER && requestAnimationFrame(gameLoop)
 }
 
 window.addEventListener('mousemove', updateJoystick)
 window.addEventListener('click', shooting);
-window.addEventListener('keydown', e =>{
-  GAME_OVER && showModal()
-  return ((e.key == ' ' || e.key == 'Enter') && !GAME_OVER && shooting())
+window.addEventListener('keydown', event =>{
+  GAME.GAME_OVER && showModal()
+  GAME.KEY = event.key
+  return (
+      (  GAME.KEY == ' ' 
+      || GAME.KEY == 'Enter')
+      && !GAME.GAME_OVER
+      && shooting()
+    )
 });
 
 gameLoop()
