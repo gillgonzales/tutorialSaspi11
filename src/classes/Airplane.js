3bfed87fcf88ff53ac7ec99e27acef4b31b4194d
Prof. Gill Gonzales <gillgonzales@ifsul.edu.br>
2023-11-08 19:24:05 -0300

    refactor: add metodo updateJoystick

--- src/classes/Airplane.js
+++ src/classes/Airplane.js
@@ -1,5 +1,5 @@
 import Model3D from "./Model3D";
-
+let that; 
 export default class Airplane extends Model3D {
 	constructor(modelPath, mtlFile, objFile, GAME) {
 		super(modelPath, mtlFile, objFile)
@@ -15,4 +15,15 @@ export default class Airplane extends Model3D {
 			this.GAME.camera.position.y = -this.model.position.y+.2
 		})
 	}
+
+	updateJoystick(event) {
+		console.log(event)
+		if (!event.buttons) {
+			this.joystick.x = event.clientX
+			this.joystick.y = event.clientY
+		} else {
+			this.joystick.x = null
+			this.joystick.y = null
+		}
+	  }
 }
